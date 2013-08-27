c76e93a492f4f18b5a2f176258ae219fa7927451
cameron <cameronjml2@gmail.com>
2013-08-27 23:05:17 +0800

    Added correct sleeping time, allowed creation of commitments and displayed information about each commitment

--- client/dashboard.js
+++ client/dashboard.js
@@ -1,5 +1,21 @@
-Template.dashboard.helpers({
-  events: function () {
-    return Events.find({});
-  }
+Template.dashboard.commitments = function () {
+    return Commitments.find({}, {sort: {title: -1}});
+};
+
+
+Template.commitment.events({
+    'click': function () {
+        Session.set("selected_commitment", this._id);
+    }
 });
+
+Template.commitment.selected = function () {
+    return Session.equals("selected_commitment", this._id) ? "selected" : '';
+};
+
+Template.dashboard.selectedEvent = function() {
+    var tmp = Commitments.findOne(Session.get("selected_commitment"));
+    // var tmpId = tmp.eventIds; // cannot find property
+    return tmp && tmp.eventIds;
+    //return Events.find(Events.ObjectID(tmp.eventIds));
+};
--- server/initialize.js
+++ server/initialize.js
@@ -1,61 +1,77 @@
 Meteor.startup(function () {
-  if (Events.find().count() === 0) {
-    
-    var date = new Date();
-	  var d = date.getDate();
-	  var m = date.getMonth();
-	  var y = date.getFullYear();
+    if (Events.find().count() === 0) { 
+        var now = moment().startOf('week');
+	var d = now.date();
+	var m = now.month();
+	var y = now.year();
+	var events = [
+	    {
+		title: 'Meeting',
+		start: new Date(y, m, d+2, 9, 30),
+		end: new Date(y, m, d+2, 10, 30),
+		allDay: false
+	    },
+	    {
+		title: 'Lunch',
+		start: new Date(y, m, d+1, 12, 0),
+		end: new Date(y, m, d+1, 14, 0),
+		allDay: false
+	    },
+	    {
+		title: 'Birthday Party',
+		start: new Date(y, m, d+3, 16, 0),
+		end: new Date(y, m, d+3, 18, 30),
+		allDay: false
+	    },
+	    {
+		title: 'Hacking',
+		start: new Date(y, m, d+5, 16, 0),
+		end: new Date(y, m, d+5, 18, 30),
+		allDay: false
+	    },	    
+            {
+		title: 'Ultimate Frisbee',
+		start: new Date(y, m, d+6, 12, 0),
+		end: new Date(y, m, d+6, 14, 0),
+		allDay: false
+	    }
+	];
 
-	  var events = [
-		  {
-			  title: 'Meeting',
-			  start: new Date(y, m, d, 9, 30),
-			  end: new Date(y, m, d, 10, 30),
-			  allDay: false
-		  },
-		  {
-			  title: 'Lunch',
-			  start: new Date(y, m, d, 12, 0),
-			  end: new Date(y, m, d, 14, 0),
-			  allDay: false
-		  },
-		  {
-			  title: 'Birthday Party',
-			  start: new Date(y, m, d+1, 19, 0),
-			  end: new Date(y, m, d+1, 22, 30),
-			  allDay: false
-		  }
-	  ];
+        for (var offset=-1; offset<7; offset++){
+            events.push({
+                title: 'Sleep',
+                start: new Date(y,m,d+offset,21,0),
+                end: new Date(y,m,d+offset+1,9,0),
+                allDay: false
+            });
+        }
 
-    var sleepStart = function (offset) {
-      return new Date(y,m,d+offset,21,0);
+        _.each(events, function (evt) {
+            Events.insert(evt);
+        });
+        
+        if (Commitments.find().count() == 0) {
+            Commitments.insert(
+                {
+                    numSessions: 3,
+                    hoursPerSession: 1,
+                    title: "Jog",
+                    eventIds: []
+                }, 
+                function (err, res) {
+                    generateEvents(res);
+                });
+
+                Commitments.insert(
+                {
+                    numSessions: 2,
+                    hoursPerSession: 2,
+                    title: "Code",
+                    eventIds: []
+                }, 
+                function (err, res) {
+                    generateEvents(res);
+                });
+        }
     }
-    var sleepEnd = function (offset) {
-      return new Date(y,m,d+offset+1,9,0);
-    }
-    _.each([0,1,2,3], function (offset) {
-      events.push({
-        title: 'Sleep',
-        start: sleepStart(offset),
-        end: sleepEnd(offset),
-        allDay: false
-      });
-    });
-
-    _.each(events, function (evt) {
-      Events.insert(evt);
-    });
-    
-    if (Commitments.find().count() == 0) {
-      Commitments.insert({
-        numSessions: 3,
-        hoursPerSession: 1,
-        title: "Jogging",
-        eventIds: []
-      }, function (err, res) {
-        generateEvents(res);
-      });
-    }
-
-  }
 });
