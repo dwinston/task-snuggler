b8acd8f1cb74e1228b9091afd2a3e7de806dfc67
cameron <cameronjml2@gmail.com>
2013-09-03 00:45:54 +0800

    Allowed deletion of events by clicking it. Tried to allow users to resize event, but it didn't work.

--- client/initialize.js
+++ client/initialize.js
@@ -9,45 +9,70 @@ var shiftTime = function(t, dayDelta, minuteDelta) {
 };
 
 Meteor.startup(function () {
-	
-	$('#calendar').fullCalendar({
-		header: {
-			left: 'prev,next today',
-			center: 'title',
-			right: 'month,agendaWeek,agendaDay'
-		},
+  
+  $('#calendar').fullCalendar({
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
     defaultView: 'agendaWeek',
     selectable: true,
     selectHelper: true,
-    select: function(start, end, allDay) {
-			var title = prompt('Event Title:');
-			if (title) {
-        Events.insert({
-          userId: Meteor.userId(),
-					title: title,
-					start: start,
-					end: end,
-					allDay: allDay
-				});
-			}
-			calendar.fullCalendar('unselect');
-		},
+    editable: true,
     contentHeight: 600,
     firstHour: 9,
-		editable: true,
+
+    // Events inserted if clicked on empty slots
+    select: function(start, end, allDay) {
+      var title = prompt('Event Title:');
+      if (title) {
+        Events.insert({
+          userId: Meteor.userId(),
+	  title: title,
+	  start: start,
+	  end: end,
+	  allDay: allDay
+	});
+      }
+      calendar.fullCalendar('unselect');
+    },
+
     events: function(start, end, callback) {
       callback(Events.find().fetch());
     },
+
+    // Delete event by clicking on it
+    eventClick: function(event, jsEvent, view) {
+      var deleteFlag = confirm
+      ('Do you really want to delete the ' + event.title + ' event?');
+      if (deleteFlag) Events.remove(event._id);
+    },
+
+    // Allow events to be moved in the calendar
     eventDrop: function(event,dayDelta,minuteDelta,allDay,revertFunc) {
       Events.remove(event._id);
       Events.insert(event);
-    }
-	});
+    },
 
-	Deps.autorun(function () {
+    // Change event duration by resize -- this doesn't work yet
+    /*
+    eventResize: function(event,dayDelta,minuteDelta,revertFunc) {
+      alert(
+        "The end date of " + event.title + "has been moved " +
+          dayDelta + " days and " +
+          minuteDelta + " minutes."
+      );
+      if (!confirm("is this okay?")) {
+        revertFunc();
+      }
+    }*/
+  });
+
+  Deps.autorun(function () {
     Events.find();
     $('#calendar').fullCalendar(
       'refetchEvents'
-		);
+    );
   });
 });
