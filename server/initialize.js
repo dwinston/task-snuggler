234304d0cfc890ed42a0e4d9d472e3fbe9c839be
Donny Winston <dwinston@alum.mit.edu>
2013-09-17 15:24:20 -0700

    Common event properties added together, to highlight the commonality

--- server/initialize.js
+++ server/initialize.js
@@ -25,66 +25,52 @@ Meteor.startup(function () {
     var y = now.year();
     var events = [
       {
-	userId: donnyId,
-        commitmentId: 0,
+	      userId: donnyId,
         title: 'Meeting',
-	start: new Date(y, m, d+2, 9, 30),
-	end: new Date(y, m, d+2, 10, 30),
-	allDay: false,
-        lastUpdated: moment()
+	      start: new Date(y, m, d+2, 9, 30),
+	      end: new Date(y, m, d+2, 10, 30)
       },
       {
-	userId: cameronId,
-        commitmentId: 0,
+	      userId: cameronId,
         title: 'Lunch',
-	start: new Date(y, m, d+1, 12, 0),
-	end: new Date(y, m, d+1, 14, 0),
-	allDay: false,
-        lastUpdated: moment()
+	      start: new Date(y, m, d+1, 12, 0),
+	      end: new Date(y, m, d+1, 14, 0)
       },
       {
-	userId: donnyId,
-        commitmentId: 0,
+	      userId: donnyId,
         title: 'Birthday Party',
-	start: new Date(y, m, d+3, 16, 0),
-	end: new Date(y, m, d+3, 18, 30),
-	allDay: false,
-        lastUpdated: moment()
+	      start: new Date(y, m, d+3, 16, 0),
+	      end: new Date(y, m, d+3, 18, 30)
       },
       {
-	userId: cameronId,
-        commitmentId: 0,
+	      userId: cameronId,
         title: 'Hacking',
-	start: new Date(y, m, d+5, 16, 0),
-	end: new Date(y, m, d+5, 18, 30),
-	allDay: false,
-        lastUpdated: moment()
+	      start: new Date(y, m, d+5, 16, 0),
+	      end: new Date(y, m, d+5, 18, 30)
       },	    
       {
-	userId: donnyId,
-        commitmentId: 0,
+	      userId: donnyId,
         title: 'Ultimate Frisbee',
-	start: new Date(y, m, d+6, 12, 0),
-	end: new Date(y, m, d+6, 14, 0),
-	allDay: false,
-        lastUpdated: moment()
+	      start: new Date(y, m, d+6, 12, 0),
+	      end: new Date(y, m, d+6, 14, 0)
       }
     ];
 
     for (var offset=-1; offset<7; offset++){
       events.push({
         userId: cameronId,
-        commitmentId: 0,
         title: 'Sleep',
         start: new Date(y,m,d+offset,21,0),
-        end: new Date(y,m,d+offset+1,9,0),
-        allDay: false,
-        lastUpdated: moment()
+        end: new Date(y,m,d+offset+1,9,0)
       });
     }
 
     _.each(events, function (evt) {
-      Events.insert(evt);
+      Events.insert(_.extend(evt, {
+        lastUpdated = moment().toDate(),
+        commitmentId: 0,
+        allDay: false
+      }));
     });
     
     if (Commitments.find().count() == 0) {
