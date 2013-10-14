96660f6ab08a183ae75159e7cabd56a2afc10528
Donny Winston <dwinston@alum.mit.edu>
2013-10-14 14:40:02 -0700

    Clarify use of package scope and file load order

--- packages/google-calendar-sync/package.js
+++ packages/google-calendar-sync/package.js
@@ -1,12 +1,13 @@
-// template with meteor/underscore package?
-// meteorite/momentjs?
-
 Package.describe({
   summary: "Sync event collections with a user's Google calendar"
 });
 
 Package.on_use(function (api) {
   api.use(['deps', 'session'], 'client');
+
+  // Files are added in order, so e.g. package-scope variables
+  // declared in initialize.js are available in subsequently
+  // added files.
   api.add_files(['initialize.js', 'fetch.js', 'insert.js', 
                  'update.js', 'remove.js'], 'client');
 
